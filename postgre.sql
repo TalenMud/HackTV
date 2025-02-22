558ffde0c0f4ae97caf9ff70187bf10b82af57af
Ewoud Van Vooren <44306474+EwoudVV@users.noreply.github.com>
2025-02-22 13:45:41 -0500

    forgor commas 💀

--- postgre.sql
+++ postgre.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     slack_id VARCHAR(50) UNIQUE NOT NULL,
     name VARCHAR(100) NOT NULL, 
-    email VARCHAR(100) NOT NULL
+    email VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
 
@@ -10,6 +10,6 @@ CREATE TABLE streams (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description TEXT,
-    created_at TIMESTAMP DEFAULT NOW()
+    created_at TIMESTAMP DEFAULT NOW(),
     owner_id INT REFERENCES users(id)
 )
