d9f3c28c8cd13943d632784bc22a79b8c157d649
Ewoud Van Vooren <44306474+EwoudVV@users.noreply.github.com>
2025-02-22 10:41:18 -0500

    update the .sql db file to make it work more

--- postgre.sql
+++ postgre.sql
@@ -3,11 +3,13 @@ CREATE TABLE users (
     slack_id VARCHAR(50) UNIQUE NOT NULL,
     name VARCHAR(100) NOT NULL, 
     email VARCHAR(100) NOT NULL
+    created_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE streams (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    description TEXT,
+    created_at TIMESTAMP DEFAULT NOW()
+    owner_id INT REFERENCES users(id)
 )
